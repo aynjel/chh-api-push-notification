dbac5acfee8dda9f58c8a3b8ee392aca5a2fc68e
Angel Ni¤o C. Ortega <ortegacanillo76@gmail.com>
2023-10-20 13:39:00 +0800

    Add project files.

---
+++ chh-api-push-notification/Controllers/NotificationController.cs
@@ -0,0 +1,27 @@
+using System.Threading.Tasks;
+using chh_api_push_notification.Models;
+using chh_api_push_notification.Service;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace net_core_api_push_notification_demo.Controllers
+{
+    [Route("api/notification")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly INotificationService _notificationService;
+        public NotificationController(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+        [Route("send")]
+        [HttpPost]
+        public async Task<IActionResult> SendNotification(NotificationModel notificationModel)
+        {
+            var result = await _notificationService.SendNotification(notificationModel);
+            return Ok(result);
+        }
+    }
+}
---
+++ chh-api-push-notification/Models/FcmNotificationSetting.cs
@@ -0,0 +1,8 @@
+﻿namespace chh_api_push_notification.Models
+{
+    public class FcmNotificationSetting
+    {
+        public string SenderId { get; set; }
+        public string ServerKey { get; set; }
+    }
+}
